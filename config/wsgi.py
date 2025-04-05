46e16d1cd3313c94674e26f959df4564dc9fc734
tfoseel <tfoseel@gmail.com>
2025-04-06 00:55:41 +0900

    Third MVP deploy

--- config/wsgi.py
+++ config/wsgi.py
@@ -3,7 +3,7 @@ import os
 if "RENDER" in os.environ:
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.prod")
 else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.base")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.dev")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
