ba43488361bf8e153d2917676f8bd25fcf5ae7b3
tfoseel <tfoseel@gmail.com>
2025-04-06 02:24:49 +0900

    Third MVP deploy

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,20 +2,14 @@ from .base import *
 import dj_database_url
 import os
 
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-host = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
-ALLOWED_HOSTS = [host] if host else ["localhost"]
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': dj_database_url.config(conn_max_age=600)
 }
 
-# whitenoise static 설정
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# 보안 설정
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
+MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
